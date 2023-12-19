3f7091b3998c9c3de7ccf3d7e7e8afbb6ba5b638
bangnokia <bangnokia@gmail.com>
2023-12-19 18:01:04 +0700

    wip

--- tests/EndpointTest.php
+++ tests/EndpointTest.php
@@ -1,7 +1,16 @@
 <?php
 
-test('endpoint is set from config', function () {
+test('alias ping2me should be Ping instance', function () {
     $instance= app('ping2me');
 
     expect($instance)->toBeInstanceOf(\Ping2Me\Php\Ping::class);
+});
+
+test('endpoint should be set from config', function () {
+    expect(\Ping2Me\Laravel\Facades\Ping::getFacadeRoot()::$endpoint)->toBe('@daudau/ping2me');
+});
+
+test('send a dummy message', function () {
+    \Ping2Me\Laravel\Facades\Ping::send("hello ping2me community on Telegram!");
+    expect(true)->toBeTrue();
 });
